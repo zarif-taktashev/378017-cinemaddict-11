ed7de34b05cfdfdb4edab42a9b8408506499eebe
Zarif_Taktashe <zarifbeta@gmail.com>
2020-04-15 21:16:24 +0600

    film-details

--- src/main.js
+++ src/main.js
@@ -3,30 +3,33 @@ import {createMenu} from "./components/menu";
 import {createFilmList} from "./components/film-list";
 import {createFilmCard} from "./components/film-card";
 import {createShowButton} from "./components/show-button";
-import {footerStatistics} from "./components/footer-statistics";
+import {createFooterStatistics} from "./components/footer-statistics";
+import {createFilmDetails} from "./components/film-details";
 
 const TASK_COUNT = 5;
 
-const render = (container, template, place) => {
+const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElement = document.querySelector(`.main`);
 const header = document.querySelector(`.header`);
 
-render(header, createUserName(), `beforeend`);
-render(siteMainElement, createMenu(), `beforeend`);
+render(header, createUserName());
+render(siteMainElement, createMenu());
 
 const films = siteMainElement.querySelector(`.films`);
-render(films, createFilmList(), `beforeend`);
+render(films, createFilmList());
 
 const filmsList = films.querySelector(`.films-list`);
 const filmsContainer = films.querySelector(`.films-list__container`);
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(filmsContainer, createFilmCard(), `beforeend`);
+  render(filmsContainer, createFilmCard());
 }
 
-render(filmsList, createShowButton(), `beforeend`);
+render(filmsList, createShowButton());
 const footer = document.querySelector(`.footer`);
-render(footer, footerStatistics(), `beforeend`);
+render(footer, createFooterStatistics());
+
+render(document.body, createFilmDetails());
